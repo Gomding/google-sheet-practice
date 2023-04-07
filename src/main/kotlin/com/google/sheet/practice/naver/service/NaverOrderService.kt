4ca764b094e6bf423200b5c64024438c0b2e2717
Gomding <backchida@gmail.com>
2023-04-07 23:31:44 +0900

    refactor: NaverOrderService의 updateRowFromNewDelivery 메서드 분리

--- src/main/kotlin/com/google/sheet/practice/naver/service/NaverOrderService.kt
+++ src/main/kotlin/com/google/sheet/practice/naver/service/NaverOrderService.kt
@@ -1,7 +1,7 @@
 package com.google.sheet.practice.naver.service
 
 import com.google.sheet.practice.googlesheet.external.GoogleSheetClient
-import com.google.sheet.practice.naver.ProductOrderStatus
+import com.google.sheet.practice.naver.domain.ProductOrderStatus
 import com.google.sheet.practice.naver.domain.NaverProductOrderDetail
 import com.google.sheet.practice.naver.external.NaverOrderClient
 import com.google.sheet.practice.naver.external.dto.NaverProductOrdersDetailRequest
@@ -12,39 +12,50 @@ class NaverOrderService(
     private val naverOrderClient: NaverOrderClient,
     private val googleSheetClient: GoogleSheetClient,
 ) {
-    fun newOrdersDetail(): List<NaverProductOrderDetail> {
-        val ordersResponse = naverOrderClient.lastChangedStatusOrders().data.lastChangeStatuses
-        val newOrders = ordersResponse.map { it.toDomain() }
-            .filter { it.isPayed() }
-        val naverProductOrdersDetailRequest = NaverProductOrdersDetailRequest(newOrders.map { it.productOrderId })
-        val ordersDetailResponse = naverOrderClient.productOrdersDetail(naverProductOrdersDetailRequest)
-        return ordersDetailResponse.data.map { it.toDomain() }
+    fun updateRowFromNewDelivery() {
+        val originProductOrderIds = this.originProductOrderIds()
+        val newProductOrderIds = this.newProductOrderIds()
+        val orderIds = originProductOrderIds.plus(newProductOrderIds).distinct()
+
+        val orderDetailsBeforeDelivery = orderDetailsBeforeDelivery(orderIds)
+        val values = orderDetailsBeforeDelivery.map { it.flatValues() }
+        val rangeForUpdate = rangeForUpdate(orderDetailsBeforeDelivery)
+        googleSheetClient.batchUpdateValues(range = rangeForUpdate, values = values)
     }
 
-    fun updateRowFromNewDelivery() {
+    private fun orderDetailsBeforeDelivery(orderIds: List<Long>): List<NaverProductOrderDetail> {
+        val naverProductOrdersDetailRequest = NaverProductOrdersDetailRequest(orderIds)
+        val ordersDetailResponse = naverOrderClient.productOrdersDetail(naverProductOrdersDetailRequest)
+        val ordersDetail = ordersDetailResponse.data.map { it.toDomain() }
+            .filter { it.productOrderStatus == ProductOrderStatus.PAYED }
+        return ordersDetail
+    }
+
+    private fun originProductOrderIds(): List<Long> {
         val originOrders = googleSheetClient.getGoogleSheetRows("naver!A2:N100")
         val originProductOrderIds = originOrders.map { it[0].toString().toLong() }
+        return originProductOrderIds
+    }
+
+    private fun rangeForUpdate(orderDetailsBeforeDelivery: List<NaverProductOrderDetail>): String {
+        val endColumn = Char(SHEET_START_COLUMN.code + COLUMN_COUNT).toString()
+        val endRow = SHEET_START_ROW + orderDetailsBeforeDelivery.size - 1
+        val range = "$SHEET_RANGE$endColumn$endRow"
+        return range
+    }
+
+    private fun newProductOrderIds(): List<Long> {
         val ordersResponse = naverOrderClient.lastChangedStatusOrders().data.lastChangeStatuses
         val newOrders = ordersResponse.map { it.toDomain() }
             .filter { it.isPayed() }
         val newProductOrderIds = newOrders.map { it.productOrderId }
-        val newOrdersDetail = this.newOrdersDetail()
-        val orderIds = originProductOrderIds.plus(newProductOrderIds).distinct()
-        val naverProductOrdersDetailRequest = NaverProductOrdersDetailRequest(orderIds)
-        val ordersDetailResponse = naverOrderClient.productOrdersDetail(naverProductOrdersDetailRequest)
-        val ordersDetail = ordersDetailResponse.data.map { it.toDomain() }.filter { it.productOrderStatus == ProductOrderStatus.PAYED }
-        // naver!A2:M3
-        val endColumn = Char(START_SHEET_COLUMN.code + COLUMN_COUNT).toString()
-        val endRow = START_SHEET_ROW + newOrdersDetail.size - 1
-        val range = "$SHEET_RANGE$endColumn$endRow"
-        val values = ordersDetail.map { it.flatValues() }
-        googleSheetClient.batchUpdateValues(range = range, values = values)
+        return newProductOrderIds
     }
 
     companion object {
         private const val SHEET_RANGE = "naver!A2:"
-        private const val START_SHEET_ROW = 2
-        private const val START_SHEET_COLUMN = 'A'
+        private const val SHEET_START_ROW = 2
+        private const val SHEET_START_COLUMN = 'A'
         private const val COLUMN_COUNT = 14
     }
 }
