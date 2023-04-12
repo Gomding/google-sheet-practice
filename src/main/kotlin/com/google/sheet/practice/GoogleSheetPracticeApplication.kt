e35739fbe233b5c2b8e42dbfedc554dea01094b8
Gomding <backchida@gmail.com>
2023-04-12 17:17:06 +0900

    feature: 쿠팡,네이버 주문 업데이트 실행 로직 작성

--- src/main/kotlin/com/google/sheet/practice/GoogleSheetPracticeApplication.kt
+++ src/main/kotlin/com/google/sheet/practice/GoogleSheetPracticeApplication.kt
@@ -1,8 +1,14 @@
 package com.google.sheet.practice
 
+import com.google.sheet.practice.common.CurrentDateTimeService
+import com.google.sheet.practice.coupang.service.CoupangOrderService
+import com.google.sheet.practice.naver.service.NaverOrderService
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -11,4 +17,17 @@ class GoogleSheetPracticeApplication {
 
 fun main(args: Array<String>) {
     runApplication<GoogleSheetPracticeApplication>(*args)
+}
+
+@Component
+class GoogleSheetApplicationRunner(
+    private val coupangOrderService: CoupangOrderService,
+    private val naverOrderService: NaverOrderService,
+    private val currentDateTimeService: CurrentDateTimeService,
+) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        val currentDateTime = currentDateTimeService.currentDateTime()
+        naverOrderService.updateRowFromNewDelivery(currentDateTime)
+        coupangOrderService.updateOrders(currentDateTime)
+    }
 }
